100775af7adb24bd88e1e0c31df2ce8e77f5310e
Li <me@wangjinli.com>
2016-08-11 20:14:35 +0800

    Add icon.

--- WCDUserDefaultsMgr.mm
+++ WCDUserDefaultsMgr.mm
@@ -18,7 +18,6 @@ static void preferencesChanged() {
 }
 
 + (instancetype)sharedUserDefaults {
-	NSLog(@"WCD: sharedUserDefaults.");
 	static id sharedInstance = nil;
 	sharedInstance = [self new];
 	return sharedInstance;
@@ -26,7 +25,6 @@ static void preferencesChanged() {
 
 - (id)init {
 	if ((self = [super init])) {
-		NSLog(@"WCD: Mgr init.");
 		CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(),
 										NULL,
 										(CFNotificationCallback)preferencesChanged,
@@ -41,15 +39,12 @@ static void preferencesChanged() {
 - (void)_preferencesChanged {
 	NSDictionary *preferences = [[NSDictionary alloc] initWithContentsOfFile:@"/var/mobile/Library/Preferences/com.wangjinli.wechatdicepb.plist"];
 	if (!preferences) {
-		NSLog(@"WCD: Read preferences Error.");
 		[self attemptSettingFallbackPrefs];
 	}
-	NSLog(@"WCD: ------Start Read Prefs.-------");
 	_diceEnabled = preferences[@"DiceEnabled"] ? [(NSNumber *)preferences[@"DiceEnabled"] boolValue] : YES;
 	_jsbEnabled = preferences[@"JsbEnabled"] ? [(NSNumber *)preferences[@"JsbEnabled"] boolValue] : NO;
 	_dicePoint = preferences[@"DicePoint"] ? [(NSNumber *)preferences[@"DicePoint"] intValue] : 6;
 	_jsbType = preferences[@"JsbType"] ? [(NSNumber *)preferences[@"JsbType"] intValue] : 1;
-	NSLog(@"WCD: Read Complete. %d, %d, %i, %i", _diceEnabled, _jsbEnabled, _dicePoint, _jsbType);
 }
 
 - (void)attemptSettingFallbackPrefs {
